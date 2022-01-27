99bbc1fd55ea9d15c812feeff2aec961e90667f5
necmigunduz <necmigunduz@gmail.com>
2022-01-27 21:54:02 +0300

    Add nodejs api structure

--- dent-api/server.js
+++ dent-api/server.js
@@ -1,25 +1,5 @@
-import express, { urlencoded, json } from 'express';
-app = express()
-port = process.env.PORT || 5000
-mongoose = require('mongoose')
-const uri = 'mongodb://localhost/Bookdb';
-Book = require('./api/models/bookModel') //created model loading here
-// mongoose instance connection url connection
-mongoose.connect(uri, {
- useNewUrlParser: true,
- useCreateIndex: true,
- useUnifiedTopology: true
- }).then(res=>{
-  console.log("Database Connected!")
- }).catch(err => {
-  console.log(Error, err.message);
-})
-app.use(urlencoded({ extended: true }))
-app.use(json());
-import routes from './api/routes/bookRoutes'; //importing route
-routes(app); //register the route
-app.get('*', (req, res)=>{
-res.status(404).send({url: req.originalUrl + ' not found'})
-})
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 5000;
 app.listen(port);
 console.log('RESTful API server started on: ' + port);
