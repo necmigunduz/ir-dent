b7112c6453d7d8a51a16b4fd004539245c66e647
necmigunduz <necmigunduz@gmail.com>
2022-01-28 20:18:47 +0300

    Add book adding form

--- client/src/components/BookEditor.js
+++ client/src/components/BookEditor.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import { withStyles, Card, CardContent, CardActions, Modal, Button, TextField } from '@material-ui/core';
+import { 
+    withStyles, 
+    Card, 
+    CardContent, 
+    CardActions, 
+    Modal, 
+    Button, 
+    TextField, 
+    InputAdornment, 
+    InputLabel, 
+    MenuItem,
+    Select
+} from '@material-ui/core';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
 import { Form, Field } from 'react-final-form';
@@ -35,7 +47,36 @@ const BookEditor = ({ classes, post, onSave, history }) => {
                 onClose={()=> history.goBack()}
                 open
             >
-                
+                <Card className={classes.modalCard}>
+                    <form onSubmit={handleSubmit}>
+                        <CardContent className={classes.modalCardContent}>
+                            <Field name="book_name">
+                                {({ input }) =><TextField label="Book Name" autoFocus {...input} />}
+                            </Field>
+                            <Field name="book_publisher">
+                                {({ input }) =><TextField label="Book Publisher" autoFocus {...input} />}
+                            </Field>
+                            <InputLabel id="category">Book Category</InputLabel>
+                            <Select
+                                labelId="category"
+                                id="category"
+                                value={category}
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={children}>Children</MenuItem>
+                                <MenuItem value={novels}>Novels</MenuItem>
+                                <MenuItem value={selfhelp}>Selfhelp</MenuItem>
+                            </Select>
+                            <Field name="price">
+                                {({ input }) =><InputAdornment label="Book Price" autoFocus {...input} />}
+                            </Field>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small" color="primary" type="submit">Save Book</Button>
+                            <Button size="small" color="danger" onClick={()=>history.goBack()}>Cancel</Button>
+                        </CardActions>
+                    </form>
+                </Card>                
             </Modal>    
         )}
     </Form>
@@ -43,4 +84,7 @@ const BookEditor = ({ classes, post, onSave, history }) => {
 };
 
 
-export default BookEditor;
+export default compose(
+    withRouter,
+    withStyles(styles),
+)(BookEditor);
