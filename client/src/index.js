11b8491978d071abf9b47e3ee04f99cc27fa9cff
necmigunduz <necmigunduz@gmail.com>
2022-01-29 20:12:34 +0300

    Adjust index.js for redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import Thunk from 'redux-thunk';
+import reducer from '../src/_reducers/index';
+
+const store = createStore(reducer, compose(applyMiddleware(Thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
