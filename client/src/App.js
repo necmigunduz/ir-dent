222e0816d7feed36ff323ad1bf8e187146e0d1fb
necmigunduz <necmigunduz@gmail.com>
2022-01-29 20:25:15 +0300

    Add dispatch to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Form from './components/Form';
+import { useDispatch } from 'react-redux';
+import { getBooks } from './_actions/books';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks);
+  }, [dispatch]);
+  
   return (
     <>
       <Form />
